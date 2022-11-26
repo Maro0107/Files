38387ca9d4af4b547c9057d766ce860b8aba0f9c
Marek Fijalkowski <gitmaro1983@gmail.com>
2022-11-26 18:28:35 +0100

    First commit

---
+++ src/main/java/FileOutputStreamExample.java
@@ -0,0 +1,24 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class FileOutputStreamExample {
+    public static void main(String[] args)throws IOException {
+
+        char chars[] = {'Q', 'W', 'E','R','T','Y'};
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream("test.txt");
+            for (char c: chars) {
+                out.write(c);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (out != null) out.close();
+        }
+
+    }
+}
